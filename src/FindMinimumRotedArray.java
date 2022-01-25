51c95ec024159ca3da24c2e65e8a7363d0374272
Tarikul Islam <ti.rasel@gmail.com>
2022-01-25 17:51:13 +0700

    added another solutions

--- src/FindMinimumRotedArray.java
+++ src/FindMinimumRotedArray.java
@@ -2,16 +2,12 @@ public class FindMinimumRotedArray {
 
     public static int getMin(int[] array, int minIndx, int maxIndx){
 
-        if(maxIndx - minIndx == 1){
-            return Math.min(array[minIndx], array[maxIndx]);
-        }
-
         if(array[minIndx] < array[maxIndx]){
             return array[minIndx];
         } else {
             int mid = (maxIndx + minIndx) / 2;
             if (array[maxIndx] > array[mid]){
-                return FindMinimumRotedArray.getMin(array, minIndx , mid);
+                return FindMinimumRotedArray.getMin(array, minIndx , mid-1);
             } else {
                 return FindMinimumRotedArray.getMin(array, mid, maxIndx);
             }
@@ -19,11 +15,36 @@ public class FindMinimumRotedArray {
 
     }
 
+    public static int finNumber(int[] arr, int l, int r, int terget){
+        int m = (l + r)/2;
+        System.out.println(m);
+        if (arr[m] == terget){
+            return m;
+        }
+
+        if(arr[m] > arr[l]){
+            // Left sorter
+            if (terget < arr[l] || terget > arr[m]){
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        } else {
+            //Right
+            if(terget < arr[m] || terget > arr[r]){
+                r = m - 1;
+            } else{
+                l = m +1;
+            }
+        }
+        return FindMinimumRotedArray.finNumber(arr, l, r, terget);
+    }
+
     public static void main(String[] atgs){
 
         int[] inp = {2, 3, 4, 5, 0, 1};
 
-        System.out.println(FindMinimumRotedArray.getMin(inp, 0, inp.length-1));
+        System.out.println(FindMinimumRotedArray.finNumber(inp, 0, inp.length-1, 5));
 
     }
 
